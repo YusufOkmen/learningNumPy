ff033c8909f9613ec4b5ac0c6fd28c641335143a
hısaeeerq@gmail.com <yusufkmen@hotmail.com>
2025-05-06 12:33:21 +0300

    Learned how to find an element in an array with indexing

---
+++ basics/arrayIndexing.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+# print(arr[4])
+# print(arr[0])
+# print(arr[0] + arr[4])
+
+array = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+# print('5th element on 2st row: ', array[1, 4])
+# print('5th element on 2st row: ', array[0, 2])
+
+arr3D = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+# print(arr3D[1, 1, 1]) 
+# print(arr3D[1, 0, -1])
